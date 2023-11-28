448ec327b36d523a6eda781ce70f7a1780894a81
bsaiswarya2816 <bsaiswarya2816@gmail.com>
2023-11-29 00:51:20 +0530

    Initial

---
+++ src/org/games/Tournament.java
@@ -0,0 +1,12 @@
+package org.games;
+
+public class Tournament {
+	
+	private void archery() {
+		System.out.println("This is an Archery");
+	}
+	private void horseRiding() {
+		System.out.println("This is Horse Riding");
+	}
+
+}
